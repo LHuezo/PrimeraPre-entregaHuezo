54343b9aff6e54a1e678a79b971403efcd411639
Huezo,Lautaro <lautarohuezo7@gmail.com>
2024-04-24 23:31:36 -0300

    ultimos arreglos

--- PrimeraPre-entregaHuezo.py
+++ PrimeraPre-entregaHuezo.py
@@ -1,28 +1,29 @@
 #Base de datos usuario-contraseña
-BD={"lauty":"password123"}
+base_de_datos={"lauty":"password123"}
 
 #Funcion para mostrar informacion
-def Mostrar_base_de_datos():
-    print (BD)
+def mostrar_base_de_datos():
+    for clave,valor in base_de_datos.items():
+        print(clave,":", valor)
 #Funcion para alamcenar informacion
 def registro_en_base_de_datos():
     user=input("Defina su nombre de usuario:")
     password=input("Genere una contraseña:")
-    while user in BD:
+    while user in base_de_datos:
         user=input("Este usuario ya esta regristrado, intente con otro:")
     else:
-        BD[user]=password
+        base_de_datos[user]=password
         print("Su usuario se registro exitosamente")
 #Funcion para login de usuarios
 def login():
     nombre_esperado=input("Ingrese su nombre de usuario:")
-    contraseña_esperada=input("Ingrese la contraseña correspondiente a su usuario:")
-    while nombre_esperado not in BD:
+    password_esperada=input("Ingrese la contraseña correspondiente a su usuario:")
+    while nombre_esperado not in base_de_datos:
         nombre_esperado=input("Este usuario no esta registrado intente otro:")
 
     intentos=3
-    while (BD[nombre_esperado] != contraseña_esperada) and (intentos > 0):
-        contraseña_esperada=input("La contraseña ingrsada no es la correcta, intente otra vez:")
+    while (base_de_datos[nombre_esperado] != password_esperada) and (intentos > 0):
+        password_esperada=input("La contraseña ingrsada no es la correcta, intente otra vez:")
         intentos -= 1
     else:
         if intentos==0:
@@ -32,19 +33,18 @@ def login():
 
 #Funcion para elgir cual de las acciones se quiere realizar
 def menu_principal():
-    print("Escriba \"Quiero registrarme\" para registrarse en la base de datos")
-    print("Escriba \"Quiero ingresar\" para logearse en la base de datos")
-    print("Escriba \"Quiero ver la informacion\" para ver la informacion en la base de datos")
+    print("Escriba el numero \"1\" para registrarse en la base de datos")
+    print("Escriba el numero \"2\" para logearse en la base de datos")
+    print("Escriba el numero \"3\" para ver la informacion en la base de datos")
     accion=input("Escriba algunas de las acciones mostradas anteriormente:")
-    while (accion.capitalize() != "Quiero registrarme") and (accion.capitalize() != "Quiero ingresar") and (accion.capitalize() != "Quiero ver la informacion"):
+    while (int(accion) != 1) and (int(accion) != 2 ) and (int(accion) != 3):
         accion=input("El comando escrito no es ninguno de los esperados. Intente de nuevo:")
     else:
-        if accion.capitalize() == "Quiero registrarme":
+        if int(accion) == 1:
             registro_en_base_de_datos()
-        elif accion.capitalize() == "Quiero ingresar":
+        elif int(accion) == 2:
             login()
         else:
-            Mostrar_base_de_datos()
+            mostrar_base_de_datos()
 
 menu_principal()
-    
